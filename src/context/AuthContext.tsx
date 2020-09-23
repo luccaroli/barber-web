fffcbab24cd9fa358c183241d5119590a3430219
luccaroli <luccaroli20@gmail.com>
2020-09-23 16:59:48 -0300

    feat:saving user and token to localstorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from 'react'
+import React, { createContext, useCallback, useState } from 'react'
 import api from '../services/api'
 
 interface SignInCredentials {
@@ -7,24 +7,45 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name: string
+  user: object
   signIn(credentials: SignInCredentials): Promise<void>
+}
+
+interface AuthState {
+  token: string
+  user: object
 }
 
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Barber:token')
+    const user = localStorage.getItem('@Barber:user')
+    
+    if (token && user) {
+      return { token, user: JSON.parse(user) }
+    }
+
+    return {} as AuthState
+  })
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email, 
       password,
     })
-    console.log(response.data)
+    
+    const { token, user } = response.data
 
+    localStorage.setItem('@Barber:token', token)
+    localStorage.setItem('@Barber:user', JSON.stringify(user))
+
+    setData({ token, user })
   }, [])
 
   return (
-    <AuthContext.Provider value={{ name: 'Lucas', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
