ebe4d779b695c385ad64a15a68fec4d7da828673
luccaroli <luccaroli20@gmail.com>
2020-09-23 01:37:24 -0300

    feat: validating form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -44,8 +44,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         defaultValue={defaultValue}
         ref={inputRef} 
         {...rest}
-        
       />
+      {error}
     </Container>
   )
 }
