370228f4b5c5e2d4066fb5f5c8788a29f336228e
luccaroli <luccaroli20@gmail.com>
2020-09-29 17:12:45 -0300

    feat:create toast hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+
 import GlobalStyles from './styles/global'
 
 import SignIn from './pages/SignIn'
-import { AuthProvider } from './context/AuthContext'
+import AppProvider from './hooks'
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
+    </AppProvider>
     
     <GlobalStyles />
   </>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,9 @@ import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
 
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../../hooks/AuthContext'
+import { useToast } from '../../hooks/ToastContext'
+
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo1.svg'
@@ -23,6 +25,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)   
   
   const { signIn } = useAuth()
+  const { addToast } = useToast()
   
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
@@ -39,16 +42,22 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       })
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password
       })
     } catch (error) {
-      const errors = getValidationErrors(error)
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error)
 
-      formRef.current?.setErrors(errors)
+        formRef.current?.setErrors(errors)
+      }
+
+      addToast()
     }
-  }, [signIn])
+
+    
+  }, [signIn, addToast])
 
   return (
     <Container>
