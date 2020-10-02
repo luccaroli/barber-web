7a07b294143223db7cb8e1a4c587e06de4c98ff2
luccaroli <luccaroli20@gmail.com>
2020-10-02 17:06:19 -0300

    feat: routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
+import { Link } from 'react-router-dom'
 
 import { useAuth } from '../../hooks/AuthContext'
 import { useToast } from '../../hooks/ToastContext'
@@ -14,7 +15,7 @@ import logoImg from '../../assets/logo1.svg'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, Background, AnimationContainer } from './styles'
 
 interface SignInFormData {
   email: string
@@ -22,7 +23,7 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
-  const formRef = useRef<FormHandles>(null)   
+  const formRef = useRef<FormHandles>(null)
   
   const { signIn } = useAuth()
   const { addToast } = useToast()
@@ -51,6 +52,8 @@ const SignIn: React.FC = () => {
         const errors = getValidationErrors(error)
 
         formRef.current?.setErrors(errors)
+
+        return
       }
 
       addToast({
@@ -66,25 +69,27 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} style={{ height: "35%" }} alt="GoBarber"/>
+        <AnimationContainer>
+          <img src={logoImg} style={{ height: "35%" }} alt="Doctor Barber"/>
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input icon={FiMail} name="email" placeholder="Email"/>
+            <Input icon={FiMail} name="email" placeholder="Email"/>
 
-          <Input icon={FiLock} name="password" type="password" placeholder="Senha"/>
+            <Input icon={FiLock} name="password" type="password" placeholder="Senha"/>
 
-          <Button type="submit">Entrar</Button>
+            <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
+            <a href="forgot">Esqueci minha senha</a>
 
-        </Form>
-        
-          <a href="login">
+          </Form>
+          
+          <Link to="signup">
             <FiLogIn />
             Criar conta
-          </a>
+          </Link>
+        </AnimationContainer>
       </Content>
 
       <Background />
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,15 +3,16 @@ import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
+import { Link } from 'react-router-dom'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo1.svg'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, AnimationContainer, Background } from './styles'
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)    
@@ -44,23 +45,25 @@ const SignUp: React.FC = () => {
       <Background />
 
       <Content>
-        <img src={logoImg} alt="GoBarber"/>
+        <AnimationContainer>
+          <img src={logoImg} style={{ height: "35%" }} alt="Doctor Barber"/>
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input icon={FiUser} name="name" placeholder="Nome"/>
-          <Input icon={FiMail} name="email" placeholder="Email"/>
+            <Input icon={FiUser} name="name" placeholder="Nome"/>
+            <Input icon={FiMail} name="email" placeholder="Email"/>
 
-          <Input icon={FiLock} name="password" type="password" placeholder="Senha"/>
+            <Input icon={FiLock} name="password" type="password" placeholder="Senha"/>
 
-          <Button type="submit">Cadastrar</Button>
-        </Form>
-        
-          <a href="login">
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+          
+          <Link to="/">
             <FiArrowLeft />
             Voltar para o logon
-          </a>
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   )
