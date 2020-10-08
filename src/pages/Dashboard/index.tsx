2a26bd4149124acec103c55a9c1e29cf264adae4
luccaroli <luccaroli20@gmail.com>
2020-10-08 19:20:43 -0300

    style: next appointment, card

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile, } from './styles';
+import { 
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import logoImg from '../../assets/logo1.svg'
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
@@ -28,6 +37,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+        <Content>
+          <Schedule>
+            <h1>Horários agendados</h1>
+            <p>
+              <span>Hoje</span>
+              <span>Dia 08</span>
+              <span>Quinta-feira</span>
+            </p>
+
+            <NextAppointment>
+              <strong>Atendimento a seguir</strong>
+              <div>
+                <img src="https://avatars0.githubusercontent.com/u/61463908?s=460&u=6b0115a003370eaaf276cbc2ed2246aab43795a8&v=4" alt="Lucas Oliveira"/>
+
+                <strong>Lucas Oliveira</strong>
+                <span>
+                  <FiClock />
+                  20:00
+                </span>
+              </div>
+            </NextAppointment>
+          </Schedule>
+          <Calendar />
+        </Content>
     </Container>
   )
 }
