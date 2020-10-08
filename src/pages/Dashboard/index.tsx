3d8a4d190539d03cb43be212d61d4730aefd89b9
luccaroli <luccaroli20@gmail.com>
2020-10-08 18:33:51 -0300

    feat: header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Header, HeaderContent, Profile, } from './styles';
+import logoImg from '../../assets/logo1.svg'
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  const { signOut, user } = useAuth()
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Doctor Barber Logo"/>
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  )
 }
 
 export default Dashboard;
