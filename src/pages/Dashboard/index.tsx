bbc54c87d7536ea9fe027c7fe825d8cba3c6c04c
luccaroli <luccaroli20@gmail.com>
2020-10-08 22:51:32 -0300

    feat: calendar/react-day-picker

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 import { 
   Container,
@@ -20,6 +22,13 @@ const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
 
   const { signOut, user } = useAuth()
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day)
+    }
+
+  }, [])
 
   return (
     <Container>
@@ -113,9 +122,32 @@ const Dashboard: React.FC = () => {
                 </div>
               </Appointment>
             </Section>
-
           </Schedule>
-          <Calendar />
+
+          <Calendar>
+            <DayPicker 
+              weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+              fromMonth={new Date()} 
+              disabledDays={[{ daysOfWeek: [0, 6] }]}
+              modifiers={{ available: { daysOfWeek: [1, 2, 3, 4, 5] } }}
+              onDayClick={handleDateChange}
+              selectedDays={selectedDate}
+              months={[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+              ]}
+            />
+          </Calendar>
         </Content>
     </Container>
   )
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+
 
 export const Container = styled.div`
   
@@ -64,7 +66,8 @@ export const Content = styled.main`
 `
 export const Schedule = styled.div`
   flex: 1;
-  margin-left: 120px;
+  margin-left: 60px;
+  margin-right: 120px;
 
   h1 {
     font-size: 36px;
@@ -206,4 +209,43 @@ export const Appointment = styled.div`
 
 export const Calendar = styled.aside`
   width: 380px;
+
+  .DayPicker {
+    background: #28262e;
+    border-radius: 10px;
+  }
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+  }
+  
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px;
+  }
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: #3e3b47;
+    border-radius: 10px;
+    color: #fff;
+  }
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, '#3e3b47')};
+  }
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+  .DayPicker-Day--disabled {
+    color: #666360 !important;
+    background: transparent !important;
+  }
+  .DayPicker-Day--selected {
+    background: #ff9000 !important;
+    border-radius: 10px;
+    color: #232129 !important;
+  }
 `
